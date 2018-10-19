8a5aa6d42364a255fd93a2c889dcc3979f06496b
AmitabhPathak <amitabhkumarpathak@gmail.com>
2018-10-19 12:16:39 +0530

    first commit

---
+++ app/src/main/java/com/amitabh/cuber/cuber5/MapActivity.java
@@ -0,0 +1,13 @@
+package com.amitabh.cuber.cuber5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
